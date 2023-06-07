11cb2245b139c52c95ae4d1dd09a9afddb4995ce
Gladtbam <49943258+Gladtbam@users.noreply.github.com>
2023-06-07 22:23:10 +0800

    添加管理员限定、私聊限定、回复消息信息提取

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Bot 自定义命令菜单
+>
+> 私聊 @BotFather, Edit Bot - Edit Commands  
+
+```text  
+help - 帮助信息(仅私聊)
+signup - 注册
+del - 删除(仅管理员)
+```
+  
--- app/tg.py
+++ app/tg.py
@@ -1,6 +1,9 @@
 from telethon import events
 from app.db import create_user, search_user, delete_user
 from app.emby import New_User, User_Policy, Password, User_delete
+from app.db import load_config
+
+admin_ids = load_config()['ADMIN_IDS']
 
 # 处理命令的装饰器
 def command_handler(command):
@@ -26,14 +29,19 @@ def register_commands(client):
         elif text.startswith('/signup'):
             await handle_signup(event,tgid)
         elif text.startswith('/del'):
-            await handle_delete(event, tgid)
-
+            if tgid in admin_ids:               # 判断是否在管理员列表中
+                await handle_delete(event, tgid)
+            else:
+                await event.respond('您非管理员, 无权执行此命令')
 
 async def handle_start(event):
     await event.respond('欢迎使用机器人！')
 
 async def handle_help(event):
-    await event.respond('这是帮助信息。')
+    if event.is_private is not True:
+        await event.respond('仅私聊')
+    else:
+        await event.respond('这是帮助信息。')
 
 async def handle_signup(event, tgid):
     user = await event.client.get_entity(tgid)  # 获取tg用户信息
@@ -56,9 +64,12 @@ async def handle_signup(event, tgid):
     
     await event.respond(message)
 
-async def handle_delete(event, tgid):
-    result = await search_user(tgid)
-    await delete_user(tgid)
+async def handle_delete(event):
+    reply_message = await event.get_reply_message()
+    reply_tgid = reply_message.sender_id
+
+    result = await search_user(reply_tgid)
+    await delete_user(reply_tgid)
 
     await User_delete(result[1])
 
